a96c5724fc316d594c875243a997793b516cbfd1
rabidhakal2-glitch <rabidhakal2@gmail.com>
2026-05-10 15:19:46 +0200

    Update NBC2025PluginSingle.cs

--- NBC2025PluginSingle.cs
+++ NBC2025PluginSingle.cs
@@ -59,29 +59,22 @@ namespace NBC2025Plugin
 
                 string nl  = Environment.NewLine;
                 string msg = "NBC 105:2025 Plugin Complete!" + nl + nl;
-                msg += "TIME PERIOD" + nl;
                 msg += "T1 = " + T1.ToString("F4") + " s" + nl;
-                msg += "T_design = " + Tdes.ToString("F4") + " s" + nl + nl;
-                msg += "SITE SPECTRA" + nl;
-                msg += "Soil=" + soil + "  Tc=" + Tc.ToString("F1") + "  Td=" + Td.ToString("F1") + nl;
+                msg += "T_design = " + Tdes.ToString("F4") + " s" + nl;
+                msg += "Soil=" + soil + " Tc=" + Tc.ToString("F1") + " Td=" + Td.ToString("F1") + nl;
                 msg += "Ch(T) = " + Ch.ToString("F4") + nl;
-                msg += "C(T)  = " + CT.ToString("F4") + nl;
-                msg += "Cs(T) = " + CsT.ToString("F4") + nl + nl;
-                msg += "DESIGN COEFFICIENTS" + nl;
+                msg += "C(T) = " + CT.ToString("F4") + nl;
                 msg += "Cd_ULS = " + CdULS.ToString("F5") + nl;
                 msg += "Cd_SLS = " + CdSLS.ToString("F5") + nl;
-                msg += "kd     = " + kd.ToString("F2") + nl + nl;
-                msg += "BASE SHEAR" + nl;
+                msg += "kd = " + kd.ToString("F2") + nl;
                 msg += "V_ULS = " + VULS.ToString("F1") + " kN" + nl;
                 msg += "V_SLS = " + VSLS.ToString("F1") + " kN" + nl + nl;
-                msg += "Patterns : EQX EQY EQX_SLS EQY_SLS" + nl;
-                msg += "Combos   : 40 combinations" + nl + nl;
-                msg += "Please re-run analysis.";
-                MessageBox.Show(msg, "NBC 105:2025 - Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                msg += "Patterns and Combos created. Re-run analysis.";
+                MessageBox.Show(msg, "NBC 105:2025", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error: " + ex.Message, "NBC 2025 Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -139,139 +132,4 @@ namespace NBC2025Plugin
             LC("S04_ULS_mX_m03Y", new string[]{D,SDL,L,EX,EY}, new double[]{1.2,1.2,0.3,-1.0,-0.3});
             LC("S05_ULS_pY_p03X", new string[]{D,SDL,L,EY,EX}, new double[]{1.2,1.2,0.3,+1.0,+0.3});
             LC("S06_ULS_pY_m03X", new string[]{D,SDL,L,EY,EX}, new double[]{1.2,1.2,0.3,+1.0,-0.3});
-            LC("S07_ULS_mY_p03X", new string[]{D,SDL,L,EY,EX}, new double[]{1.2,1.2,0.3,-1.0,+0.3});
-            LC("S08_ULS_mY_m03X", new string[]{D,SDL,L,EY,EX}, new double[]{1.2,1.2,0.3,-1.0,-0.3});
-
-            LC("O01_09DL_pEX", new string[]{D,SDL,EX}, new double[]{0.9,0.9,+1.0});
-            LC("O02_09DL_mEX", new string[]{D,SDL,EX}, new double[]{0.9,0.9,-1.0});
-            LC("O03_09DL_pEY", new string[]{D,SDL,EY}, new double[]{0.9,0.9,+1.0});
-            LC("O04_09DL_mEY", new string[]{D,SDL,EY}, new double[]{0.9,0.9,-1.0});
-
-            LC("W01_Wind_pWX",   new string[]{D,SDL,L,WX}, new double[]{1.2,1.2,1.2,+1.6});
-            LC("W02_Wind_mWX",   new string[]{D,SDL,L,WX}, new double[]{1.2,1.2,1.2,-1.6});
-            LC("W03_Wind_pWY",   new string[]{D,SDL,L,WY}, new double[]{1.2,1.2,1.2,+1.6});
-            LC("W04_Wind_mWY",   new string[]{D,SDL,L,WY}, new double[]{1.2,1.2,1.2,-1.6});
-            LC("W05_Uplift_pWX", new string[]{D,SDL,WX},   new double[]{0.9,0.9,+1.6});
-            LC("W06_Uplift_mWX", new string[]{D,SDL,WX},   new double[]{0.9,0.9,-1.6});
-            LC("W07_Uplift_pWY", new string[]{D,SDL,WY},   new double[]{0.9,0.9,+1.6});
-            LC("W08_Uplift_mWY", new string[]{D,SDL,WY},   new double[]{0.9,0.9,-1.6});
-
-            LC("SLS0_DL_SDL_LL",  new string[]{D,SDL,L},         new double[]{1.0,1.0,1.0});
-            LC("SLS1_pX_p03Y", new string[]{D,SDL,L,EXs,EYs}, new double[]{1.0,1.0,0.3,+1.0,+0.3});
-            LC("SLS2_pX_m03Y", new string[]{D,SDL,L,EXs,EYs}, new double[]{1.0,1.0,0.3,+1.0,-0.3});
-            LC("SLS3_mX_p03Y", new string[]{D,SDL,L,EXs,EYs}, new double[]{1.0,1.0,0.3,-1.0,+0.3});
-            LC("SLS4_mX_m03Y", new string[]{D,SDL,L,EXs,EYs}, new double[]{1.0,1.0,0.3,-1.0,-0.3});
-            LC("SLS5_pY_p03X", new string[]{D,SDL,L,EYs,EXs}, new double[]{1.0,1.0,0.3,+1.0,+0.3});
-            LC("SLS6_pY_m03X", new string[]{D,SDL,L,EYs,EXs}, new double[]{1.0,1.0,0.3,+1.0,-0.3});
-            LC("SLS7_mY_p03X", new string[]{D,SDL,L,EYs,EXs}, new double[]{1.0,1.0,0.3,-1.0,+0.3});
-            LC("SLS8_mY_m03X", new string[]{D,SDL,L,EYs,EXs}, new double[]{1.0,1.0,0.3,-1.0,-0.3});
-
-            EC("ENV_ULS_Seismic",     new string[]{"S01_ULS_pX_p03Y","S02_ULS_pX_m03Y","S03_ULS_mX_p03Y","S04_ULS_mX_m03Y","S05_ULS_pY_p03X","S06_ULS_pY_m03X","S07_ULS_mY_p03X","S08_ULS_mY_m03X"});
-            EC("ENV_ULS_Overturning", new string[]{"O01_09DL_pEX","O02_09DL_mEX","O03_09DL_pEY","O04_09DL_mEY"});
-            EC("ENV_ULS_Wind",        new string[]{"W01_Wind_pWX","W02_Wind_mWX","W03_Wind_pWY","W04_Wind_mWY","W05_Uplift_pWX","W06_Uplift_mWX","W07_Uplift_pWY","W08_Uplift_mWY"});
-            EC("ENV_SLS_Seismic",     new string[]{"SLS1_pX_p03Y","SLS2_pX_m03Y","SLS3_mX_p03Y","SLS4_mX_m03Y","SLS5_pY_p03X","SLS6_pY_m03X","SLS7_mY_p03X","SLS8_mY_m03X"});
-            EC("ENV_ALL_ULS",         new string[]{"G1_DL1.5_SDL1.5_LL1.5","G2_DL1.5_SDL1.5_RL1.5","G3_DL1.2_SDL1.2_LL1.6","G4_DL1.2_SDL1.2_RL1.6","ENV_ULS_Seismic","ENV_ULS_Overturning","ENV_ULS_Wind"});
-            EC("ENV_ALL_SLS",         new string[]{"SLS0_DL_SDL_LL","ENV_SLS_Seismic"});
-        }
-
-        void LC(string name, string[] pats, double[] sfs)
-        {
-            try
-            {
-                _m.RespCombo.Add(name, 0);
-                eCNameType cType = eCNameType.LoadCase;
-                for (int i=0; i<pats.Length; i++)
-                    _m.RespCombo.SetCaseList(name, ref cType, pats[i], sfs[i]);
-            }
-            catch {}
-        }
-
-        void EC(string name, string[] subs)
-        {
-            try
-            {
-                _m.RespCombo.Add(name, 1);
-                eCNameType cType = eCNameType.LoadCombo;
-                for (int i=0; i<subs.Length; i++)
-                    _m.RespCombo.SetCaseList(name, ref cType, subs[i], 1.0);
-            }
-            catch {}
-        }
-    }
-
-    public class InputForm : Form
-    {
-        public double H,Z,I,Rmu,OmegaU,OmegaS,W,kt;
-        public int    NStory;
-        public string Soil,Dead,SDL,Live,RoofLive,WindX,WindY;
-
-        NumericUpDown nudH,nudZ,nudI,nudRmu,nudOmegaU,nudOmegaS,nudW,nudkt,nudNStory;
-        ComboBox cbSoil;
-        TextBox  tbDead,tbSDL,tbLive,tbRL,tbWX,tbWY;
-
-        public InputForm()
-        {
-            Text            = "NBC 105:2025 - Seismic Inputs";
-            Width           = 480;
-            Height          = 600;
-            StartPosition   = FormStartPosition.CenterScreen;
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-            MaximizeBox     = false;
-            Font            = new System.Drawing.Font("Segoe UI", 9f);
-
-            Panel pnl = new Panel();
-            pnl.Dock       = DockStyle.Fill;
-            pnl.AutoScroll = true;
-            pnl.Padding    = new Padding(14);
-            Controls.Add(pnl);
-            int y = 10;
-
-            Label title = new Label();
-            title.Text      = "NBC 105:2025  |  ETABS 19 Plugin";
-            title.Font      = new System.Drawing.Font("Segoe UI",11f,System.Drawing.FontStyle.Bold);
-            title.ForeColor = System.Drawing.Color.FromArgb(26,79,114);
-            title.Location  = new System.Drawing.Point(0,y);
-            title.Width     = 440;
-            title.Height    = 24;
-            pnl.Controls.Add(title);
-            y += 32;
-
-            Sec(pnl,"Building Geometry",ref y);
-            nudH  = Num(pnl,"Total Height H (m)",         ref y,11.48,1,500,2);
-            nudkt = Num(pnl,"kt (RC=0.075  Steel=0.085)", ref y,0.075,0.01,0.5,3);
-
-            Sec(pnl,"Site Parameters",ref y);
-            cbSoil = Cmb(pnl,"Soil Type (Vs30)",ref y,
-                new string[]{"A - Hard Rock  >800 m/s",
-                             "B - Rock  360-800 m/s",
-                             "C - Soft Rock  180-360 m/s",
-                             "D - Soft Soil  <180 m/s  [Kathmandu]"},3);
-            nudZ = Num(pnl,"Z - Zone Factor (0.35=Kathmandu)",  ref y,0.35,0.05,1.0,2);
-            nudI = Num(pnl,"I - Importance (1.0 / 1.25 / 1.5)", ref y,1.0,0.5,2.0,2);
-
-            Sec(pnl,"Structural System",ref y);
-            nudRmu    = Num(pnl,"Rmu - Ductility Factor",    ref y,4.0,1.0,8.0,1);
-            nudOmegaU = Num(pnl,"OmegaU - ULS Overstrength", ref y,1.5,1.0,3.0,2);
-            nudOmegaS = Num(pnl,"OmegaS - SLS Overstrength", ref y,1.25,1.0,3.0,2);
-
-            Sec(pnl,"Seismic Weight and Stories",ref y);
-            nudW      = Num(pnl,"W - Seismic Weight (kN)",   ref y,15341,1,99999999,0);
-            nudNStory = Num(pnl,"Number of Storeys",          ref y,3,1,100,0);
-
-            Sec(pnl,"Load Pattern Names",ref y);
-            tbDead = Txt(pnl,"Dead Load", ref y,"Dead");
-            tbSDL  = Txt(pnl,"SDL",       ref y,"SDL");
-            tbLive = Txt(pnl,"Live Load", ref y,"Live");
-            tbRL   = Txt(pnl,"Roof Live", ref y,"LiveRoof");
-            tbWX   = Txt(pnl,"Wind X",    ref y,"WX");
-            tbWY   = Txt(pnl,"Wind Y",    ref y,"WY");
-
-            y += 8;
-            Button ok = new Button();
-            ok.Text         = "Apply to ETABS";
-            ok.DialogResult = DialogResult.OK;
-            ok.Location     = new System.Drawing.Point(0,y);
-            ok.Width        = 200;
-            ok.Height       = 30;
-            ok.BackColor    = System.Drawing.Color.FromArgb(26,79,114);
-            ok.For
+            LC("S07_ULS_mY_p03X", new string[]{D,SDL,L,EY,EX}, new double[]{1
